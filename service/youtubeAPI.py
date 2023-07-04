a0435c48befdf647d8e1e688c12ea4172e8ee82e
김민석 <kimminseok@gimminseog-ui-MacBookAir.local>
2023-07-04 15:40:45 +0900

    add: connect with youtubeAPI

--- service/youtubeAPI.py
+++ service/youtubeAPI.py
@@ -1,16 +1,18 @@
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import isodate
-from youtube_transcript_api import YouTubeTranscriptApi
-from dotenv import load_dotenv
-import os
-from typing import List
-import pickle
-from datetime import datetime, timedelta
 from google_auth_oauthlib.flow import InstalledAppFlow
-import os
 from google.auth.transport.requests import Request
+from youtube_transcript_api import YouTubeTranscriptApi
+
+from schema.schemas import RecipeCreate
+
+from dotenv import load_dotenv
+from typing import List
+from datetime import datetime
 import pickle
+import isodate
+import pickle
+import os
 
 
 def convert_seconds_to_time_str(seconds: float) -> str:
@@ -45,10 +47,14 @@ def get_auth():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 credentials_path,
-                os.getenv("SCOPES"),
+                scopes=[
+                    "openid",
+                    "https://www.googleapis.com/auth/userinfo.email",
+                    "https://www.googleapis.com/auth/userinfo.profile",
+                    "https://www.googleapis.com/auth/youtube.force-ssl",
+                ],
             )
-            creds = flow.run_local_server(port=0)
-
+            creds = flow.run_local_server()
         with open("auto_token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
@@ -84,13 +90,16 @@ class YoutubeAPI:
         )
 
         video_ids = []
+        short_ids = []
 
         for video in videos["items"]:
             if video["id"]["kind"] == "youtube#video":
                 video_id = video["id"]["videoId"]
                 video_ids.append("%s" % video_id)
-
-        return video_ids
+            if video["id"]["kind"] == "youbue#short":
+                short_id = video["id"]["videoID"]
+                short_ids.append("%s" % short_id)
+        return video_ids, short_ids
 
     def get_RecipeInfo(self, channelName: str) -> List[object]:
         try:
@@ -103,60 +112,112 @@ class YoutubeAPI:
             return None
 
     # 영상 Id를 통해 필요한 데이터 수집
-    def getVideoInfoById(self, videoIds: List[str]) -> List[object]:
-        video_info = []
-
-        for videoId in videoIds:
-            getInfo = (
-                self.youtube.videos()
-                .list(
-                    part="snippet,statistics,topicDetails,contentDetails,status",
-                    id=videoId,
-                )
-                .execute()
+    def getVideoInfoById(self, videoId: str) -> RecipeCreate:
+        youtube_response = (
+            self.youtube.videos()
+            .list(
+                part="snippet,statistics,topicDetails,contentDetails,status",
+                id=videoId,
             )
+            .execute()
+        )
 
-            view_count = getInfo["items"][0]["statistics"]["viewCount"]
-            like_count = getInfo["items"][0]["statistics"]["likeCount"]
+        view_count = youtube_response["items"][0]["statistics"]["viewCount"]
+        like_count = youtube_response["items"][0]["statistics"]["likeCount"]
 
-            title = getInfo["items"][0]["snippet"]["title"]
-            published_at = getInfo["items"][0]["snippet"]["publishedAt"]
-            descriptioin = getInfo["items"][0]["snippet"]["description"]
-            thumnail = getInfo["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        title = youtube_response["items"][0]["snippet"]["title"]
+        published_at = youtube_response["items"][0]["snippet"]["publishedAt"]
+        descriptioin = youtube_response["items"][0]["snippet"]["description"]
+        thumnail = youtube_response["items"][0]["snippet"]["thumbnails"]["default"][
+            "url"
+        ]
+        channel_id = youtube_response["items"][0]["snippet"]["channelId"]
+        duration = youtube_response["items"][0]["contentDetails"]["duration"]
+        parsed_duration = isodate.parse_duration(duration)
+        run_time = parsed_duration.total_seconds()
 
-            duration = getInfo["items"][0]["contentDetails"]["duration"]
-            parsed_duration = isodate.parse_duration(duration)
-            run_time = parsed_duration.total_seconds()
+        if run_time > 60:  # 쇼츠는 tags가 없는 것도 있던데...?
+            tags = youtube_response["items"][0]["snippet"]["tags"]
+        else:
+            tags = []
+        category = youtube_response["items"][0]["topicDetails"]["topicCategories"]
 
-            if run_time > 60:  # 쇼츠는 tags가 없는 것도 있던데...?
-                tags = getInfo["items"][0]["snippet"]["tags"]
-            else:
-                tags = []
+        transcript_list = YouTubeTranscriptApi.list_transcripts(videoId)
+        transcript = transcript_list.find_transcript(["ko", "en"])
+        if transcript == None:
+            script = []
+        else:
+            script = transcript.fetch()
+        processed_data = process_data(script)
 
-            category = getInfo["items"][0]["topicDetails"]["topicCategories"]
+        recipe_data = RecipeCreate(
+            youtubeVideoId=videoId,
+            youtubeTitle=title,
+            youtubeChannel=channel_id,
+            youtubeViewCount=view_count,
+            difficulty="",
+            category="",
+            youtubePublishedAt=published_at,
+            youtubeLikeCount=like_count,
+            youtubeTag=("${0}" % (tags)),
+            youtubeDescription=descriptioin,
+            youtubeCaption=("${0}" % (processed_data)),
+        )
 
-            transcript_list = YouTubeTranscriptApi.list_transcripts(videoId)
-            transcript = transcript_list.find_transcript(["ko", "en"])
-            if transcript == None:
-                script = []
-            else:
-                script = transcript.fetch()
-            processed_data = process_data(script)
+        return recipe_data
 
-            video_info_seed = {
-                "id": videoId,
-                "title": title,
-                "publishedAt": published_at,
-                "description": descriptioin,
-                "thumnail": thumnail,
-                "category": category,
-                "tags": tags,
-                "viewCount": view_count,
-                "like_count": like_count,
-                "run_time": run_time,
-                "script": processed_data,
-            }
+    def get_subscriptions(self) -> List[str]:
+        # 유저가 구독한 채널 정보를 가져옵니다.
+        try:
+            channelLists = []
+            nextPageToken = None
+            while True:
+                subscriptions_request = self.youtube.subscriptions().list(
+                    part="snippet", mine=True, maxResults=50, pageToken=nextPageToken
+                )
+                subscription_response = subscriptions_request.execute()
 
-            video_info.append(video_info_seed)
+                subscriptions = subscription_response["items"]
+                for subscription in subscriptions:
+                    channel_title = subscription["snippet"]["title"]
+                    channel_id = subscription["snippet"]["resourceId"]["channelId"]
+                    channel_thumnail = subscription["snippet"]["thumbnails"]["default"][
+                        "url"
+                    ]
+                    channelLists.append(
+                        {
+                            "id": channel_id,
+                            "title": channel_title,
+                            "thumbnail": channel_thumnail,
+                        }
+                    )
+                nextPageToken = subscription_response.get("nextPageToken")
 
-        return video_info
+                if not nextPageToken:
+                    break
+        except HttpError as error:
+            print(f"An HTTP error {error.resp.status} occurred: {error.content}")
+            return None
+
+        return channelLists
+
+    def get_videos_by_date(
+        self, published_after: datetime, subscriptions: List[str]
+    ) -> List[str]:
+        # 특정 날짜와 구독자 목록이 주어지면 이를 바탕으로 해당 날짜 이후에 나온 구독자의 영상이 나온다
+        video_ids = []
+        for subscription in subscriptions:
+            search_request = self.youtube.search().list(
+                part="snippet",
+                channelId=subscription,
+                type="video",
+                maxResults=50,
+                publishedAfter=published_after,
+            )
+            search_response = search_request.execute()
+
+            video_id = search_response["items"][0]["id"]["videoId"]
+
+            video_ids.append(video_id)
+
+        return video_ids
